70ec8cf9f3c34553de329113d920ba663616b6ec
Zhouuuw <yizhouwen@Nill-Mac.local>
2019-01-30 19:44:26 -0700

    asd

--- app/src/main/java/com/example/a301team19/lab4/Shape.java
+++ app/src/main/java/com/example/a301team19/lab4/Shape.java
@@ -25,9 +25,11 @@ package com.example.a301team19.lab4;
 public abstract class Shape {
     public int x;
     public int y;
+    public int z;
 
     Shape(int x, int y){
         this.x  = x;
         this.y = y;
+        this.z = 0;
     }
 }
